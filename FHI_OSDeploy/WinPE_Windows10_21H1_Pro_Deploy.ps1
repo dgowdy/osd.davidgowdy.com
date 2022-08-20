ecd8d69e5ab072d4829eae460660e8aa307eeba5
David Gowdy <34044429+dgowdy@users.noreply.github.com>
2022-08-20 10:54:55 -0400

    Update WinPE_Windows10_21H1_Pro_Deploy.ps1

--- FHI_OSDeploy/WinPE_Windows10_21H1_Pro_Deploy.ps1
+++ FHI_OSDeploy/WinPE_Windows10_21H1_Pro_Deploy.ps1
@@ -1,6 +1,6 @@
 #================================================
 #   OSDCloud Task Sequence
-#   Windows 10 21H1 Pro en-us OEM
+#   Windows 10 21H2 Pro en-us Volume
 #   No Autopilot
 #   No Office Deployment Tool
 #================================================
@@ -13,38 +13,22 @@ Import-Module OSD -Force
 #   [OS] Start-OSDCloud with Params
 #================================================
 $Params = @{
-    OSBuild = "21H1"
+    OSBuild = "21H2"
     OSEdition = "Pro"
     OSLanguage = "en-us"
-    OSLicense = "OEM"
+    OSLicense = "Volume"
     SkipAutopilot = $true
     SkipODT = $true
 }
 Start-OSDCloud @Params
 #================================================
 #   WinPE PostOS Sample
-#   AutopilotOOBE Offline Staging
-#================================================
-Install-Module AutopilotOOBE -Force
-Import-Module AutopilotOOBE -Force
-
-$Params = @{
-    Title = 'OSDeploy Autopilot Registration'
-    GroupTag = 'Enterprise'
-    GroupTagOptions = 'Development','Enterprise'
-    Hidden = 'AddToGroup','AssignedComputerName','AssignedUser','PostAction'
-    Assign = $true
-    Run = 'NetworkingWireless'
-    Docs = 'https://autopilotoobe.osdeploy.com/'
-}
-AutopilotOOBE @Params
-#================================================
-#   WinPE PostOS Sample
 #   OOBEDeploy Offline Staging
 #================================================
 $Params = @{
-    Autopilot = $true
-    RemoveAppx = "CommunicationsApps","OfficeHub","People","Skype","Solitaire","Xbox","ZuneMusic","ZuneVideo"
+    Autopilot = $false
+    AddNetFX3 = $true
+    RemoveAppx = $true
     UpdateDrivers = $true
     UpdateWindows = $true
 }
@@ -78,37 +62,6 @@ REM start "Start-OOBEDeploy" PowerShell -NoL -C Start-OOBEDeploy -AddNetFX3 -Upd
 exit
 '@
 $SetCommand | Out-File -FilePath "C:\Windows\OOBEDeploy.cmd" -Encoding ascii -Force
-#================================================
-#   WinPE PostOS
-#   Set AutopilotOOBE CMD.ps1
-#================================================
-$SetCommand = @'
-@echo off
-
-:: Set the PowerShell Execution Policy
-PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
-
-:: Add PowerShell Scripts to the Path
-set path=%path%;C:\Program Files\WindowsPowerShell\Scripts
-
-:: Open and Minimize a PowerShell instance just in case
-start PowerShell -NoL -W Mi
-
-:: Install the latest AutopilotOOBE Module
-start "Install-Module AutopilotOOBE" /wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-
-:: Start-AutopilotOOBE
-:: There are multiple example lines. Make sure only one is uncommented
-:: The next line assumes that you have a configuration saved in C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json
-start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE
-:: The next line is how you would apply a CustomProfile
-REM start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -CustomProfile OSDeploy
-:: The next line is how you would configure everything from the command line
-REM start "Start-AutopilotOOBE" PowerShell -NoL -C Start-AutopilotOOBE -Title 'OSDeploy Autopilot Registration' -GroupTag Enterprise -GroupTagOptions Development,Enterprise -Assign
-
-exit
-'@
-$SetCommand | Out-File -FilePath "C:\Windows\Autopilot.cmd" -Encoding ascii -Force
 #================================================
 #   PostOS
 #   Restart-Computer
